4ecfb9a3fefa63d43a8fcee66cb0a78de9c125d7
aaronguostudio <aaronguostudio@gmail.com>
2020-04-05 18:58:29 -0600

    add table adapters and manager methods

--- WebBrowser.Logic/BookmarkManager.cs
+++ WebBrowser.Logic/BookmarkManager.cs
@@ -3,10 +3,34 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using WebBrowser.Data.BookmarkDataSetTableAdapters;
 
 namespace WebBrowser.Logic
 {
     public class BookmarkManager
     {
+        public static void Add(BookmarkItem bookmarkItem)
+        {
+            var adapter = new BookmarksTableAdapter();
+            adapter.Insert(bookmarkItem.Title, bookmarkItem.URL);
+        }
+
+        public static List<BookmarkItem> GetItems()
+        {
+            var adapter = new BookmarksTableAdapter();
+            var results = new List<BookmarkItem>();
+            var rows = adapter.GetData();
+            
+            foreach(var row in rows)
+            {
+                var item = new BookmarkItem();
+                item.Id = row.Id;
+                item.Title = item.Title;
+                item.URL = item.URL;
+                results.Add(item);
+            }
+
+            return results;
+        }
     }
 }
--- WebBrowser.Logic/HistoryManager.cs
+++ WebBrowser.Logic/HistoryManager.cs
@@ -3,10 +3,35 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using WebBrowser.Data.HistoryDataSetTableAdapters;
 
 namespace WebBrowser.Logic
 {
     public class HistoryManager
     {
+        public static void AddItem(HistoryItem historyItem)
+        {
+            var adapter = new HistoryTableAdapter();
+            adapter.Insert(historyItem.Title, historyItem.URL, historyItem.Date);
+        }
+
+        public static List<HistoryItem> GetItems()
+        {
+            var adapter = new HistoryTableAdapter();
+            var results = new List<HistoryItem>();
+            var rows = adapter.GetData();
+
+            foreach(var row in rows)
+            {
+                var item = new HistoryItem();
+                item.Id = row.Id;
+                item.Title = row.Title;
+                item.URL = row.URL;
+                item.Date = row.Date;
+                results.Add(item);
+            }
+
+            return results;
+        }
     }
 }
